c91846131d5416ebd65438afd56bdfc6315b4620
chris <thcrnk@gmail.com>
2017-12-18 11:46:57 +0200

    Proxy migrations

--- migrations/2_marketplace_contracts.js
+++ migrations/2_marketplace_contracts.js
@@ -1,18 +1,24 @@
 let MarketplaceProxy = artifacts.require("./../Marketplace/MarketplaceProxy.sol");
 let Marketplace = artifacts.require("./../Marketplace/Marketplace.sol");
+let IMarketplace = artifacts.require("./../Marketplace/IMarketplace.sol");
 
 let PropertyProxy = artifacts.require("./../Property/PropertyProxy.sol");
 let Property = artifacts.require("./../Property/Property.sol");
+let IProperty = artifacts.require("./../Property/IProperty.sol");
 
 module.exports = async function (deployer) {
 
     await deployer.deploy(Property);
-    let PropertyInstance = await Property.deployed();
-    await deployer.deploy(PropertyProxy, PropertyInstance.address);
-    await PropertyInstance.init();
+    let PropertyImpl = await Property.deployed();
+    await deployer.deploy(PropertyProxy, PropertyImpl.address);
+    let PropertyContract = await PropertyProxy.deployed();
+    PropertyContract = await IProperty.at(PropertyContract.address);
+    await PropertyContract.init();
 
     await deployer.deploy(Marketplace);
-    let MarketplaceInstance = await Marketplace.deployed();
-    await deployer.deploy(MarketplaceProxy, MarketplaceInstance.address);
-    await MarketplaceInstance.init(PropertyInstance.address);
+    let MarketplaceImpl = await Marketplace.deployed();
+    await deployer.deploy(MarketplaceProxy, MarketplaceImpl.address);
+    let MarketplaceContract = await MarketplaceProxy.deployed();
+    MarketplaceContract = await IMarketplace.at(MarketplaceContract.address);
+    await MarketplaceContract.init(PropertyContract.address);
 };
